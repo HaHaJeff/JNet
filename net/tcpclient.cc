0afa114846a6dd5bf51ba8099f8702db82d9d965
jeff <327767852@qq.com>
2019-05-23 15:48:43 +0800

    update rpc

--- net/tcpclient.cc
+++ net/tcpclient.cc
@@ -1,2 +1,20 @@
 #include "tcpclient.h"
 #include "channel.h"
+
+TcpClient::TcpClient(EventLoop* loop,
+                     const Ip4Addr& local,
+                     const Ip4Addr& peer)
+    : loop_(loop),
+      conn_(loop, local, peer)
+{
+    TRACE("TcpClient create!");
+}
+
+void TcpClient::Connect() {
+}
+
+void TcpClient::Disconnect() {
+
+}
+
+TcpClient::~TcpClient() {}
--- net/tcpclient.h
+++ net/tcpclient.h
@@ -5,21 +5,23 @@
 
 class TcpClient : Noncopyable {
 public:
-    TcpClient(EventLoop* loop, const Ip4Addr& local, const Ip4Addr& peer) {}
-    ~TcpClient() {}
+    TcpClient(EventLoop* loop, const Ip4Addr& local, const Ip4Addr& peer);
+    ~TcpClient();
 
-    void Connect() {}
-    void Disconnnect() {}  
+    void Connect();
+    void Disconnect();
+    EventLoop* GeLoop() const { return loop_; }
 
     void SetReadCallback(const TcpCallBack& cb) { readCb_ = cb; }
     void SetWriteCallback(const TcpCallBack& cb) { writeCb_ = cb; }
     void SetStateCallback(const TcpCallBack& cb) { stateCb_ = cb; }
 
 private:
-    void NewConnection(int sockfd) {}
-    void RemoveConnection(const TcpConnPtr& conn) {}
+    void NewConnection(int sockfd);
+    void RemoveConnection(const TcpConnPtr& conn);
 
 private:
+    EventLoop* loop_;
     TcpConn conn_;
     TcpCallBack readCb_;
     TcpCallBack writeCb_;
